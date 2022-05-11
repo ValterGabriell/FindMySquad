39ac4659545a7295e1de05c681c8fc3e2a3c304d
Valter Gabriel <vgabrielbri@hotmail.com>
2022-05-10 23:13:15 -0300

    Iniciando a configuração da edit requisicao activity

---
+++ app/src/main/java/com/example/findmysquad/View/EditarRequisicaoActivity.kt
@@ -0,0 +1,11 @@
+package com.example.findmysquad.View
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditarRequisicaoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_editar_requisicao)
+    }
+}
---
+++ app/src/main/java/com/example/findmysquad/ViewModel/EditarRequisicaoViewModel.kt
@@ -0,0 +1,10 @@
+package com.example.findmysquad.ViewModel
+
+import com.example.findmysquad.Repository.EditRequistionRepository.EditRequisitonRepository
+
+class EditarRequisicaoViewModel(private val repository: EditRequisitonRepository) {
+
+
+
+
+}
